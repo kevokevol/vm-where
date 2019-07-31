866ef3ffb01d8ce61a10f17e22a2460698a91533
Andrew Chen <achen226@gmail.com>
2019-07-31 16:55:33 -0700

    added db connector

---
+++ native_db_cnt.py
@@ -0,0 +1,55 @@
+import datetime
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+
+# Use a service account CHANGE THIS TO THE PATH OF YOUR OWN CERTIFICATE
+cred = credentials.Certificate('/Users/andrewchen/Androo/firebase_test/roketto-dan-firebase-adminsdk-1tvwb-a77cbc7880.json')
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+def readFromDB(collection):
+    """
+    Prints all the entries in a specified collection in the database
+    """
+    users_ref = db.collection(collection)
+    docs = users_ref.stream()
+
+    for doc in docs:
+        print(u'{} => {}'.format(doc.id, doc.to_dict()))
+
+
+def insertIntoDB(collection, numCars):
+    """
+    Inserts current time and number of cars into the database with the respective
+    collection and document categories
+    """
+    
+    gen_id = db.collection('prom').where(u'count', u'>=', 0)
+    g_id = gen_id.stream()
+
+    for gid in g_id:
+        index = gid.to_dict()['count']
+    
+    doc = 'test-' + str(index)
+    index = index + 1
+    
+    # increment count
+    gen_id = db.collection('prom').document('numEntries')
+    gen_id.set({
+        'count': index
+    })
+
+    doc_ref = db.collection(collection).document(doc)
+    doc_ref.set({
+        'time': datetime.datetime.now(),
+        'index': index,
+        'cars': numCars
+    })
+
+
+
+
+
+
